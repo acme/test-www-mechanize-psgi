2126f901cdd4a06c022925a9c36bc00233255130
Chia-liang Kao <clkao@clkao.org>
2010-01-06 16:01:13 +0800

    Use prepare_request so default_headers are added properly.
    This also makes the request_prepare and response_done hooks called,
    so cookies jars are handled without the current hack.

--- lib/Test/WWW/Mechanize/PSGI.pm
+++ lib/Test/WWW/Mechanize/PSGI.pm
@@ -32,9 +32,8 @@ sub simple_request {
     my $uri = $request->uri;
     $uri->scheme('http')    unless defined $uri->scheme;
     $uri->host('localhost') unless defined $uri->host;
-    $self->cookie_jar->add_cookie_header($request) if $self->cookie_jar;
 
-    my $env = $request->to_psgi;
+    my $env = $self->prepare_request($request)->to_psgi;
     my $response;
     try {
         $response = HTTP::Response->from_psgi( $self->{app}->($env) );
@@ -46,7 +45,7 @@ sub simple_request {
         $response->content_type('');
     };
     $response->request($request);
-    $self->cookie_jar->extract_cookies($response) if $self->cookie_jar;
+    $self->run_handlers("response_done", $response);
     return $response;
 }
 
